c0c81d70711a857ba7dedae814b230ab2314dde1
Egor Yurtaev <yurtaev.egor@gmail.com>
2013-02-06 21:36:20 +0700

    fix the mixed tabs and spaces

--- pi-radio-mpd.py
+++ pi-radio-mpd.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
- # Copyright 2013 Umputun
+# Copyright 2013 Umputun
 
- # This file is part of Pi-Radio.
+# This file is part of Pi-Radio.
 
- # Pi-Radio is free software: you can redistribute it and/or modify
- # it under the terms of the GNU General Public License as published by
- # the Free Software Foundation, either version 3 of the License, or
- # (at your option) any later version.
+# Pi-Radio is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
- # Pi-Radio is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
+# Pi-Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
- # You should have received a copy of the GNU General Public License
- # along with Pi-Radio.  If not, see http://www.gnu.org/licenses/.
+# You should have received a copy of the GNU General Public License
+# along with Pi-Radio.  If not, see http://www.gnu.org/licenses/.
 
 
 import web, subprocess, json, signal, time, mpd, collections
@@ -46,108 +46,108 @@ index_page = open('index.html', 'r').read()
 
 class index:
     def GET(self):
-	return index_page
+        return index_page
 
 class icon:
     def GET(self):
-	web.header('Content-Type', 'image/png')
-	return open('pi-radio.png', 'r').read()
+        web.header('Content-Type', 'image/png')
+        return open('pi-radio.png', 'r').read()
 
 class list:
     def GET(self):
-	print STATIONS
-	web.header('Content-Type', 'application/json')
-	return (json.dumps({'response' : {'list': collections.OrderedDict(sorted(STATIONS.items()))} }, separators=(',',':') ))
+        print STATIONS
+        web.header('Content-Type', 'application/json')
+        return (json.dumps({'response' : {'list': collections.OrderedDict(sorted(STATIONS.items()))} }, separators=(',',':') ))
 
 class volume:
 
     def GET(self, volume_level): #level between 0 and 10
 
-	mpc = mc.get_client()
+        mpc = mc.get_client()
 
-	level = int(mc.get_client().status()['volume'])
-	if volume_level:
-	    if int(volume_level) == 0: level = 0
-	    else: level = int(volume_level)*10 # 80 + int(volume_level) * 2
-	    mpc.setvol(level)
-	else:
-	    volume_level = level/10 # (level - 80) / 2
-	print "volume level=%d, %s" % (level, volume_level)
+        level = int(mc.get_client().status()['volume'])
+        if volume_level:
+            if int(volume_level) == 0: level = 0
+            else: level = int(volume_level)*10 # 80 + int(volume_level) * 2
+            mpc.setvol(level)
+        else:
+            volume_level = level/10 # (level - 80) / 2
+        print "volume level=%d, %s" % (level, volume_level)
 
-	mc.release_client()
+        mc.release_client()
 
-	web.header('Content-Type', 'application/json')
-	return (json.dumps({'response' :  {'level': volume_level} }, separators=(',',':') ))
+        web.header('Content-Type', 'application/json')
+        return (json.dumps({'response' :  {'level': volume_level} }, separators=(',',':') ))
 
     def POST(self, volume_level): return self.GET(volume_level)
 
 class play:
     def GET(self, station):
-	print "play station="+ station.encode('utf-8')
-	client = mc.get_client()
-	client.stop()
-	found_id = [sid for sid, st_name in mc.ids.items() if st_name == station][0]
-	print "play id=%s" % found_id
-	client.playid(found_id)
-	mc.release_client()
-	web.header('Content-Type', 'application/json')
-	web.ctx.status = '201 Created'
-	return (json.dumps({'response' :  {'station': STATIONS[station]} }, separators=(',',':') ))
+        print "play station="+ station.encode('utf-8')
+        client = mc.get_client()
+        client.stop()
+        found_id = [sid for sid, st_name in mc.ids.items() if st_name == station][0]
+        print "play id=%s" % found_id
+        client.playid(found_id)
+        mc.release_client()
+        web.header('Content-Type', 'application/json')
+        web.ctx.status = '201 Created'
+        return (json.dumps({'response' :  {'station': STATIONS[station]} }, separators=(',',':') ))
 
     def POST(self, volume_level): return self.GET(volume_level)
 
 class stop:
     def GET(self):
-	print "stop"
-	client = mc.get_client()
-	client.stop()
-	mc.release_client()
-	web.header('Content-Type', 'application/json')
-	return (json.dumps({'response' :  {'result' : 1} }, separators=(',',':') ))
+        print "stop"
+        client = mc.get_client()
+        client.stop()
+        mc.release_client()
+        web.header('Content-Type', 'application/json')
+        return (json.dumps({'response' :  {'result' : 1} }, separators=(',',':') ))
 
     def POST(self): return self.GET()
 
 class status:
     def GET(self):
-	mpd_status = mc.get_client().status()
-	print "status = " + str(mpd_status)
-	mc.release_client()
-#	volume = (int(mpd_status['volume']) - 80) / 2
-	volume = int(mpd_status['volume']) / 10
-	web.header('Content-Type', 'application/json')
-	if mpd_status['state'] == 'play':
-	    station_name = mc.ids[mpd_status['songid']]
-	    return (json.dumps({'response' :  {'status' : 'play', 'station' : station_name, 'volume' : volume} }, separators=(',',':') ))
-	else:
-	    return (json.dumps({'response' :  {'status' : 'stop', 'volume' : volume} }, separators=(',',':') ))
+        mpd_status = mc.get_client().status()
+        print "status = " + str(mpd_status)
+        mc.release_client()
+        #	volume = (int(mpd_status['volume']) - 80) / 2
+        volume = int(mpd_status['volume']) / 10
+        web.header('Content-Type', 'application/json')
+        if mpd_status['state'] == 'play':
+            station_name = mc.ids[mpd_status['songid']]
+            return (json.dumps({'response' :  {'status' : 'play', 'station' : station_name, 'volume' : volume} }, separators=(',',':') ))
+        else:
+            return (json.dumps({'response' :  {'status' : 'stop', 'volume' : volume} }, separators=(',',':') ))
 
 class mpd_controller:
     def __init__(self, stations):
-	self.client = mpd.MPDClient()
+        self.client = mpd.MPDClient()
 
-	try:
-	    self.client.connect("localhost", 6600)
-	except mpd.ConnectionError:
-	    print "already connected"
+        try:
+            self.client.connect("localhost", 6600)
+        except mpd.ConnectionError:
+            print "already connected"
 
-	self.client.clear()
-	self.ids = {}
-	for (st_name, st_url) in stations.items():
-	    self.ids[self.client.addid(st_url)] = st_name
-	self.client.disconnect()
+        self.client.clear()
+        self.ids = {}
+        for (st_name, st_url) in stations.items():
+            self.ids[self.client.addid(st_url)] = st_name
+        self.client.disconnect()
 
     def get_client(self):
-	try:
-	    self.client.connect("localhost", 6600)
-	except mpd.ConnectionError:
-	    print "already connected"
-	return self.client
+        try:
+            self.client.connect("localhost", 6600)
+        except mpd.ConnectionError:
+            print "already connected"
+        return self.client
 
     def release_client(self):
-	try:
-    	    self.client.disconnect()
-	except mpd.ConnectionError:
-	    print "can't disconnect"
+        try:
+            self.client.disconnect()
+        except mpd.ConnectionError:
+            print "can't disconnect"
 
 
 mc = mpd_controller(STATIONS)
